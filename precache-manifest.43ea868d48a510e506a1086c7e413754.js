f9e3406a08b6cf10c467b04010dc9ce1085075a6
Maya Lake <abcdcookiemonster@gmail.com>
2020-10-09 14:54:47 -0700

    Updates

---
+++ precache-manifest.43ea868d48a510e506a1086c7e413754.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b670da704d454e89244a612a55d96d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cc372dc52a50a04ccd9",
+    "url": "/static/css/main.eb712daa.chunk.css"
+  },
+  {
+    "revision": "f28ef28eb6121c05ebc6",
+    "url": "/static/js/2.d0e3c888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0e3c888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc372dc52a50a04ccd9",
+    "url": "/static/js/main.723e4a18.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ad28f20f88b7ba3efd5380054e9e2588",
+    "url": "/static/media/Maya_Lake_Resume.ad28f20f.pdf"
+  },
+  {
+    "revision": "bda2919f68f66397ba948f99c625cc6a",
+    "url": "/static/media/background.bda2919f.jpg"
+  },
+  {
+    "revision": "00d8adf223871c0d1681b8739f1e642c",
+    "url": "/static/media/email_icon.00d8adf2.png"
+  },
+  {
+    "revision": "3d4580262ccc40197750019eaaa7d7be",
+    "url": "/static/media/headshot.3d458026.jpg"
+  },
+  {
+    "revision": "e7e0d9e07ff2465a730c816958b5a3fb",
+    "url": "/static/media/linkedin_icon.e7e0d9e0.png"
+  },
+  {
+    "revision": "48ae60689e7b9fae30130d85c8cd36d3",
+    "url": "/static/media/resume.48ae6068.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mayalake.github.io/precache-manifest.824a0b35a37ea49146b56aba2b796686.js"
+  "/precache-manifest.43ea868d48a510e506a1086c7e413754.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mayalake.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
